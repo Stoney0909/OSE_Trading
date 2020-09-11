db277e1046a7c1e1cb5b1885abe1d8310dc493f7
Zhibin He <zhibin.he1@snhu.edu>
2020-09-12 01:52:05 +0800

    Login page, Signup page, UI finish.

--- app.py
+++ app.py
@@ -2,14 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def login_page():
-    return render_template('login_page.html')
+    return render_template('Login_page.html')
 
-@app.route('/signup')
+@app.route('/Signup')
 def signup_page():
-    return render_template('signup_page.html')
+    return render_template('Signup_page.html')
+
+@app.route('/Findpassword')
+def forgetPassword_page():
+    return render_template('ForgetPassword_page.html')
+
+@app.route('/Home')
+def home_page():
+    return render_template('Home_page.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
