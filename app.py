95e13bfd3c92122271b3e78ffb3802c4dfb00d91
ethanhe1999 <zhibin.he1@snhu.edu>
2020-09-14 04:53:14 +0800

    Home page templete done

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home_page():
-    return render_template('Home_page.html')
-
-@app.route('/login')
 def login_page():
     return render_template('Login_page.html')
 
@@ -17,6 +13,10 @@ def signup_page():
 @app.route('/ForgetPassword')
 def forgetPassword_page():
     return render_template('ForgetPassword_page.html')
+
+@app.route('/Home')
+def home_page():
+    return render_template('Home_page.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
